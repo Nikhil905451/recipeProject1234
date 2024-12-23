62961e6e48d7b21adfceb2bfd02becad4c0f6fe5
Nikhil905451 <nikhiljaiswal8370@gmail.com>
2024-12-23 11:10:04 +0530

    Create jekyll-gh-pages.yml

--- Reccipe/recipe1/models.py
+++ Reccipe/recipe1/models.py
@@ -7,6 +7,6 @@ class Recepie(models.Model):
     recipe_name = models.CharField(max_length=100)
     recipe_description = models.TextField()
     recipe_image = models.ImageField(upload_to="recipe")
-
+    recipe_view_count = models.IntegerField(default=1)
 
 
